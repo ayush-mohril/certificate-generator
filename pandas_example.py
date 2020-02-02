cfa29da56119ddd74d37b254005cae9e19ae651c
Ayush Mohril <mohrilayush@gmail.com>
2020-02-02 11:55:14 +0530

    changed name of file

--- pandas_example.py
+++
@@ -1,8 +0,0 @@
-import pandas as pa
-df =pa.read_excel('example.xlsx')
-print(df)
-##for index,row in df.iterrows():
-    #name=row['name']
-    #branch=row['branch']
-    #year=row['year']
-    #print(name,branch,year)
